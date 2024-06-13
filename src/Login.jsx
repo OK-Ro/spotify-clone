873f8e58faf6e8b228186d9ad27f5879c1fcc9ae
OK-Ro <o.robert1994@hotmail.com>
2024-06-13 20:31:48 +0200

    spotify-login

---
+++ src/Login.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const LoginContainer = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: black;
+`;
+
+const SpotifyLogo = styled.img`
+  width: auto;
+  height: 200px;
+`;
+
+const LoginButton = styled.button`
+  padding: 20px;
+  border-radius: 99px;
+  font-weight: 600;
+  color: white;
+  text-decoration: none;
+  background-color: #1db954;
+`;
+
+function Login() {
+  return (
+    <LoginContainer>
+      <SpotifyLogo
+        src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
+        alt="Spotify logo"
+      />
+      <LoginButton>LOGIN WITH SPOTIFY</LoginButton>
+    </LoginContainer>
+  );
+}
+
+export default Login;
